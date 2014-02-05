fa446f495011f788db13e830630126c7b89b7f4a
vjkanhai <kanhai@ualberta.ca>
2014-02-04 19:11:38 -0700

    Added all changed files

--- src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
+++ src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
@@ -1,17 +1,18 @@
 package ca.ualberta.cs.comput301.currentlocation;
 
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class CurrentLocationActivity extends Activity {
 	private static final String GPS_MOCK_PROVIDER = "GpsMockProvider";
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -19,13 +20,44 @@ public class CurrentLocationActivity extends Activity {
 		
 		// Obtain LocationManager service and set up location update request.
 		// To Do
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
 	}
 
 	
 	// Retrieve location updates through LocationListener interface
-	private final LocationListener locationListener = new LocationListener(){
+	private final LocationListener locationListener = new LocationListener()
+	{
 	// To Do: override the four methods.
-	}	
+		@Override
+		public void onLocationChanged(Location location)
+		{
+			TextView tv = (TextView) findViewById(R.id.myLocationText);
+			if(location != null)
+			{
+				double lat = location.getLatitude();
+				double lng = location.getLongitude();
+				Date date = new Date(location.getTime());
+				tv.setText("Your location is:"+"\nLatitude:"+lat+"\nLongitude:"+lng+"\nat:"+date.toString());
+			}
+			else
+			{
+				tv.setText("No location information");
+			}
+		}
+		public void onProviderDisabled (String provider)
+		{
+			
+		}
+		public void onProviderEnabled (String provider)
+		{
+			
+		}
+		public void onStatusChanged (String provider, int status, Bundle extras)
+		{
+			
+		}
+	};
 
 	
 	@Override
